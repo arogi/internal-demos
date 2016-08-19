6dbf2a25bc24db5a2d8394ae81dc0423e304c278
Alan Glennon <glennon@gmail.com>
2016-08-19 00:13:34 -0700

    Update build.sh
    network-tsp does not require a configuration setup. removed.

--- build.sh
+++ build.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-DIRECTORY=conf
-if [ ! -d "$DIRECTORY" ]; then
-
-  git clone \
-    --depth=1 \
-    --recurse-submodules \
-    --single-branch \
-    --branch=master https://github.com/arogi/network-tsp.git
-fi
-
 docker build \
   --tag arogi/network-tsp \
   --force-rm \
